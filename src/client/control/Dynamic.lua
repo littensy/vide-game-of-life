64c5b69e24d3ff04f9039bc9875d97e2915ebd7c
Richard <richerd.rdc@gmail.com>
2023-08-27 19:27:08 -0700

    refactor: simplify dynamic control

--- src/client/control/Dynamic.lua
+++ src/client/control/Dynamic.lua
@@ -1,42 +1,27 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local vide = require(ReplicatedStorage.modules.vide)
-local Cleanup = require(script.Parent.Cleanup)
 
 type Component<T> = (T) -> any
 
-local function unmount(elements)
-	local container = vide.create "Folder" { elements }
-	container:Destroy()
-end
-
 local function Dynamic<T>(props: {
 	component: vide.Source<Component<T>?>,
-} & T): { any }
-	local set = vide.source({})
-	local destroyed = false
+} & T)
+	local container = vide.create "Folder" {}
+
+	local function update(component: Component<T>?)
+		container:ClearAllChildren()
+
+		if component then
+			vide.apply(container) { component(props) }
+		end
+	end
 
 	vide.watch(function()
-		set({ props.component() })
+		task.defer(update, props.component())
 	end)
 
-	return {
-		vide.values(set, function(render)
-			local elements = render(props)
-
-			vide.cleanup(function()
-				if not destroyed then
-					unmount(elements)
-				end
-			end)
-
-			return elements
-		end),
-
-		Cleanup(function()
-			destroyed = true
-		end),
-	}
+	return container
 end
 
 return Dynamic
--- src/client/control/If.lua
+++ src/client/control/If.lua
@@ -3,7 +3,7 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local vide = require(ReplicatedStorage.modules.vide)
 local Dynamic = require(script.Parent.Dynamic)
 
-local function If(condition: vide.Source<unknown>, render: () -> any): { any }
+local function If(condition: vide.Source<unknown>, render: () -> any)
 	return Dynamic {
 		component = function()
 			return if condition() then render else nil
