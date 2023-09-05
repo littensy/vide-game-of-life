2add58c73ff267a1eb5c0e6682f61e1118b2b3d7
Richard <richerd.rdc@gmail.com>
2023-09-05 00:05:46 -0700

    refactor: remove useless type cast

--- src/client/control/dynamic.lua
+++ src/client/control/dynamic.lua
@@ -3,7 +3,7 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local vide = require(ReplicatedStorage.modules.vide)
 
 local function dynamic<T>(source: vide.Source<T>, render: (value: T) -> ())
-	local children = vide.source({} :: any)
+	local children = vide.source({})
 
 	vide.watch(function()
 		local value = source()
