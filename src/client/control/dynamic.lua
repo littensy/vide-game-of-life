00a338f209ef3c0f88a481175f3537bca7d5bd4d
Richard <richerd.rdc@gmail.com>
2023-09-05 00:03:00 -0700

    refactor: default to empty table

--- src/client/control/dynamic.lua
+++ src/client/control/dynamic.lua
@@ -3,11 +3,12 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local vide = require(ReplicatedStorage.modules.vide)
 
 local function dynamic<T>(source: vide.Source<T>, render: (value: T) -> ())
-	local children = vide.source(nil :: any)
+	local children = vide.source({} :: any)
 
 	vide.watch(function()
 		local value = source()
 
+		-- todo: use vide.untrack
 		task.defer(function()
 			children({ render(value) })
 		end)
